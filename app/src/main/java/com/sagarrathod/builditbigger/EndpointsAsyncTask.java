44845c73acdccc838043826aaaab682341babb13
Sagar Rathod <sagar100rathod@gmail.com>
2016-10-09 23:19:14 +0530

    Added Gradle Custom task for starting GCE local development server, rnning all testcases and then stopping the GCE server.

--- app/src/main/java/com/sagarrathod/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/sagarrathod/builditbigger/EndpointsAsyncTask.java
@@ -1,17 +1,12 @@
 package com.sagarrathod.builditbigger;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
-import android.util.Log;
 
 import com.example.samsung.builditbigger.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-import com.sagarrathod.andlib.DisplayActivity;
-import com.sagarrathod.commons.Utils;
 
 import java.io.IOException;
 
@@ -31,6 +26,7 @@ public class EndpointsAsyncTask extends AsyncTask<String,Void, String> {
 
     @Override
     protected String doInBackground(String... params) {
+        String rootUrl = "https://tactile-acrobat-144517.appspot.com/_ah/api/";
 
         if (myApiService == null) {  // Only do this once
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
@@ -38,7 +34,7 @@ public class EndpointsAsyncTask extends AsyncTask<String,Void, String> {
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("https://tactile-acrobat-144517.appspot.com/_ah/api/")
+                    .setRootUrl(rootUrl)
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
